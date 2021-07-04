c67375f533c45a98957c932c6055abb82928ab25
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-07-04 19:20:27 -0300

    bug fix

---
+++ eslint-prettier-backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.json({
+    message: "Hello World!",
+  });
+});
+
+app.listen(3333, () => {
+  console.log("App running on port 3333!");
+});
